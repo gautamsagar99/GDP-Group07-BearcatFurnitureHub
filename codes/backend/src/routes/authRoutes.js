f0b8ab3a8585909811d6cc27a977d6ebc9c8b6f8
Gautam Sagar <gautamsagar.mallela99@gmail.com>
2023-09-08 16:13:57 -0500

    Added JWT  Validation for all routes

--- codes/backend/src/routes/authRoutes.js
+++ codes/backend/src/routes/authRoutes.js
@@ -1,5 +1,40 @@
 // src/routes/authRoutes.js
 const express = require("express");
+const jwt = require("jsonwebtoken");
+
+// Define your middleware for token verification
+function verifyToken(req, res, next) {
+  // console.log("verified");
+  // next();
+  if (
+    req.originalUrl === "/login" ||
+    req.originalUrl === "/signup" ||
+    req.originalUrl === "/register" ||
+    req.originalUrl === "/test"
+  ) {
+    return next(); // Skip token verification for these routes
+  }
+
+  const token = req.headers.authorization; // Get the token from the request headers
+
+  if (!token) {
+    return res.status(401).json({ message: "Authorization token is missing" });
+  }
+
+  try {
+    // Verify the token
+    const decoded = jwt.verify(token, "your-secret-key"); // Use the same secret key you used to sign the token
+
+    // Attach the decoded user information to the request for later use
+    req.user = decoded;
+
+    // Continue to the next middleware or route handler
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+}
+
 const {
   login,
   signup,
@@ -10,6 +45,9 @@ const {
 } = require("../controllers/authController");
 
 const router = express.Router();
+
+// Apply the verifyToken middleware to all routes
+router.use(verifyToken);
 
 router.post("/login", login);
 router.post("/signup", signup);
