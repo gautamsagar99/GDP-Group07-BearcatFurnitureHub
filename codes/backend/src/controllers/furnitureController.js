aba10eafd69a4bb57940b3b944133488051d76f8
Gautam Sagar <gautamsagar.mallela99@gmail.com>
2023-09-10 14:38:38 -0500

    Added Few Home Page API's

--- codes/backend/src/controllers/furnitureController.js
+++ codes/backend/src/controllers/furnitureController.js
@@ -40,10 +40,86 @@ async function getAllFurniture(req, res) {
   }
 }
 
+// Function to get one furniture record by ID
+async function getFurnitureById(req, res) {
+  try {
+    const { id } = req.params; // Get the ID from the request parameters
+
+    // Find the furniture record by ID in the database
+    const furniture = await Furniture.findByPk(id);
+
+    if (!furniture) {
+      return res.status(404).json({ message: "Furniture not found" });
+    }
+
+    res.status(200).json(furniture);
+  } catch (error) {
+    console.error("Error retrieving furniture by ID:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+// Function to update a specific furniture record by ID
+async function updateFurniture(req, res) {
+  try {
+    const { id } = req.params;
+    const { name, condition, yearsUsed, imageUrl, category } = req.body;
+
+    // Retrieve the furniture record by ID from the database
+    const furniture = await Furniture.findByPk(id);
+
+    if (!furniture) {
+      return res.status(404).json({ message: "Furniture not found" });
+    }
+
+    // Update the furniture record
+    furniture.name = name;
+    furniture.condition = condition;
+    furniture.yearsUsed = yearsUsed;
+    furniture.imageUrl = imageUrl;
+    furniture.category = category;
+
+    // Save the updated furniture record to the database
+    await furniture.save();
+
+    res
+      .status(200)
+      .json({ message: "Furniture updated successfully", furniture });
+  } catch (error) {
+    console.error("Error updating furniture:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+// Function to delete a specific furniture record by ID
+async function deleteFurniture(req, res) {
+  try {
+    const { id } = req.params;
+
+    // Retrieve the furniture record by ID from the database
+    const furniture = await Furniture.findByPk(id);
+
+    if (!furniture) {
+      return res.status(404).json({ message: "Furniture not found" });
+    }
+
+    // Delete the furniture record
+    await furniture.destroy();
+
+    res.status(200).json({ message: "Furniture deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting furniture:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
 // Add more controller functions for updating and deleting furniture records as needed
 
 module.exports = {
   createFurniture,
   getAllFurniture,
+  getFurnitureById,
+  updateFurniture,
+  deleteFurniture,
   // Add other functions here
 };
--- codes/backend/src/routes/furnitureRoutes.js
+++ codes/backend/src/routes/furnitureRoutes.js
@@ -7,6 +7,9 @@ const router = express.Router();
 // Define routes for furniture-related functionality
 router.post("/create-furniture", furnitureController.createFurniture);
 router.get("/get-furniture", furnitureController.getAllFurniture);
+router.get("/get-furniture/:id", furnitureController.getFurnitureById);
+router.put("/update-furniture/:id", furnitureController.updateFurniture); // Update furniture by ID
+router.delete("/delete-furniture/:id", furnitureController.deleteFurniture);
 // Add more routes for updating and deleting furniture records as needed
 
 module.exports = router;
