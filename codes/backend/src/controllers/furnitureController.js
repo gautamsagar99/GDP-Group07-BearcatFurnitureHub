6d334175cf8c2e577a04c5c8a5be7e36311d630d
Gautam Sagar <gautamsagar.mallela99@gmail.com>
2023-09-17 14:57:07 -0500

    Added Image URL logic for furniture

--- codes/backend/src/controllers/furnitureController.js
+++ codes/backend/src/controllers/furnitureController.js
@@ -2,6 +2,9 @@
 const { Furniture } = require("../models/furniture"); // Import your Furniture model
 const { User } = require("../models/user");
 
+// const AWS = require("aws-sdk"); // Import AWS SDK
+// const s3 = new AWS.S3();
+
 /// Function to create a new furniture record
 async function createFurniture(req, res) {
   try {
@@ -42,6 +45,63 @@ async function createFurniture(req, res) {
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
+
+// async function createFurniture(req, res) {
+//   try {
+//     const {
+//       name,
+//       condition,
+//       yearsUsed,
+//       image, // Change the name from imageUrl to image
+//       category,
+//       furniture_description,
+//       userEmail, // Add userEmail to the request body
+//     } = req.body;
+
+//     // Find the user by email to get the user ID
+//     const user = await User.findOne({ where: { email: userEmail } });
+
+//     if (!user) {
+//       return res.status(404).json({ message: "User not found" });
+//     }
+
+//     // Generate a unique filename for the image using the furniture name
+//     const imageKey = `${name}-${Date.now()}.jpg`;
+
+//     // Configure the S3 parameters for uploading the image
+//     const s3Params = {
+//       Bucket: name, // Use the furniture name as the bucket name
+//       Key: imageKey, // Use the unique filename for the image
+//       Body: image, // The image file
+//       ACL: "public-read", // Make the uploaded image public-readable
+//     };
+
+//     // Upload the image to S3
+//     const s3UploadResponse = await s3.upload(s3Params).promise();
+
+//     // Get the URL of the uploaded image
+//     const imageUrl = s3UploadResponse.Location;
+
+//     // Create a new furniture record in the database with today's date for Donated_date
+//     const furniture = await Furniture.create({
+//       name,
+//       condition,
+//       yearsUsed,
+//       imageUrl, // Use the S3 URL for the image
+//       category,
+//       furniture_description, // New column
+//       Donated_date: new Date(), // Set Donated_date to today's date
+//       user_id: user.id, // Use the retrieved user ID
+//     });
+
+//     res
+//       .status(201)
+//       .json({ message: "Furniture created successfully", furniture });
+//   } catch (error) {
+//     console.error("Error creating furniture:", error);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// }
 
 // Function to get all available furniture records
 async function getAllFurniture(req, res) {
@@ -177,6 +237,117 @@ async function updateFurniture(req, res) {
     res.status(500).json({ message: "Internal Server Error" });
   }
 }
+
+// Function to update a specific furniture record by ID
+// async function updateFurniture(req, res) {
+//   try {
+//     const { id } = req.params;
+//     const {
+//       name,
+//       furniture_condition,
+//       years_used,
+//       image, // Change the name from image_url to image
+//       furniture_type,
+//       furniture_description,
+//       status,
+//       userEmail, // Add userEmail to the request body
+//     } = req.body;
+
+//     // Retrieve the furniture record by ID from the database
+//     const furniture = await Furniture.findByPk(id);
+
+//     if (!furniture) {
+//       return res.status(404).json({ message: "Furniture not found" });
+//     }
+
+//     // Check if the status is changing from "requested" to "cancelled"
+//     if (furniture.status === "requested" && status === "cancelled") {
+//       // Update the furniture status to "available"
+//       furniture.status = "available";
+
+//       // Find the user by email to get the user ID
+//       const user = await User.findOne({ where: { email: userEmail } });
+
+//       if (!user) {
+//         return res.status(404).json({ message: "User not found" });
+//       }
+
+//       // Delete the user record from the Requested table
+//       await Requested.destroy({
+//         where: { user_id: user.id, furniture_id: id },
+//       });
+//     }
+
+//     // Update the furniture record properties if provided
+//     if (name) {
+//       furniture.name = name;
+//     }
+//     if (furniture_condition) {
+//       furniture.furniture_condition = furniture_condition;
+//     }
+//     if (years_used) {
+//       furniture.years_used = years_used;
+//     }
+
+//     // Handle image update
+//     if (image) {
+//       // Generate a unique filename for the image using the furniture name
+//       const imageKey = `${furniture.name}-${Date.now()}.jpg`;
+
+//       // Configure the S3 parameters for uploading the image
+//       const s3Params = {
+//         Bucket: furniture.name, // Use the furniture name as the bucket name
+//         Key: imageKey, // Use the unique filename for the image
+//         Body: image, // The new image file
+//         ACL: "public-read", // Make the uploaded image public-readable
+//       };
+
+//       // Upload the new image to S3
+//       const s3UploadResponse = await s3.upload(s3Params).promise();
+
+//       // Get the URL of the uploaded image and update the furniture's image_url
+//       furniture.image_url = s3UploadResponse.Location;
+//     }
+
+//     if (furniture_type) {
+//       furniture.furniture_type = furniture_type;
+//     }
+//     if (furniture_description) {
+//       furniture.furniture_description = furniture_description;
+//     }
+//     if (status === "requested" || status === "closed") {
+//       furniture.status = status;
+//     }
+
+//     // Save the updated furniture record to the database
+//     await furniture.save();
+
+//     // Check if the status is changed to "requested"
+//     if (status === "requested") {
+//       // Find the user by email to get the user ID
+//       const user = await User.findOne({ where: { email: userEmail } });
+
+//       if (!user) {
+//         return res.status(404).json({ message: "User not found" });
+//       }
+
+//       const requestDate = new Date();
+//       // Add a record to the Requested table
+//       await Requested.create({
+//         user_id: user.id,
+//         furniture_id: id,
+//         request_date: requestDate,
+//       });
+//     }
+
+//     res
+//       .status(200)
+//       .json({ message: "Furniture updated successfully", furniture });
+//   } catch (error) {
+//     console.error("Error updating furniture:", error);
+//     res.status(500).json({ message: "Internal Server Error" });
+//   }
+// }
 
 // Function to delete a specific furniture record by ID
 // Function to update the status of a specific furniture record to "deleted" by ID
