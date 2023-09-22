ec027f7a72596b0d9bbf9570f509e018252359fa
Mamatha-Mallela <111670297+Mamatha-Mallela@users.noreply.github.com>
2023-09-22 01:21:15 -0500

    Complete Search Functionality

--- codes/frontend/src/pages/HomePage/Home.js
+++ codes/frontend/src/pages/HomePage/Home.js
@@ -6,6 +6,10 @@ import Navbar from '../../components/Navbar';
 
 function Home() {
   const [furnitureData, setFurnitureData] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const resetSearchQuery = () => {
+    setSearchQuery(''); // Reset the search query to an empty string
+  };
 
   useEffect(() => {
     axios.get('http://localhost:5000/get-furniture')
@@ -18,20 +22,72 @@ function Home() {
       });
   }, []);
 
+  const filteredFurniture = furnitureData.filter(item => {
+    const lowerSearchQuery = searchQuery.toLowerCase().trim(); // Trim whitespace from the search query
+    const lowerItemName = item.name.toLowerCase().trim(); // Trim whitespace from the item's name
+    const lowerItemType = item.furniture_type.toLowerCase().trim();
+    const furnitureCondition = item.furniture_condition.toLowerCase().trim(); // Trim whitespace from furniture_condition
+
+    const searchQueryWords = lowerSearchQuery.split(' '); // Split lowerSearchQuery into individual words
+
+    const conditionMatches = lowerSearchQuery.includes(furnitureCondition); // Check if the furniture condition matches the search query
+    console.log("conditionMatches",conditionMatches);
+
+    const nameMatches = searchQueryWords.every(word => {
+      const wordRegex = new RegExp(`\\b${word}\\b`, 'i'); // Match whole word with case-insensitivity
+      return wordRegex.test(lowerItemName);
+    });
+    console.log("nameMatches",nameMatches);
+
+    const typeMatches = searchQueryWords.some(word => lowerItemType.includes(word))
+    console.log("typeMatches",typeMatches);
+
+    if(nameMatches){
+      return nameMatches;
+    }
+
+    else if(conditionMatches && nameMatches){
+      return conditionMatches && nameMatches;
+    }
+
+    else if(conditionMatches && typeMatches){
+      return conditionMatches && typeMatches;
+    }
+    return false;
+  });
+
   return (
     <div>
-      <Navbar/>
+      <Navbar onSearch={setSearchQuery} onResetSearch={resetSearchQuery}/> {/* Pass setSearchQuery to Navbar */}
+       {filteredFurniture.length === 0 ? ( // Check if no products are found
+        <div style={{ textAlign: 'center', marginTop: '250px' }}>
+          <p>Sorry! No products found.</p>
+        </div>
+      ) : (
       <div className="card-container">
-        {furnitureData.map((item, index) => (
-          <FurnitureCard
-            key={index}
-            productId={item.id}
-            imageSrc={item.image_url}
-            label={item.name}
-            description={item.name}
-          />
-        ))}
+        {searchQuery === '' ? (
+          furnitureData.map((item, index) => (
+            <FurnitureCard
+              key={index}
+              productId={item.id}
+              imageSrc={item.image_url}
+              label={item.name}
+              description={item.furniture_description}
+            />
+          ))
+        ) : (
+          filteredFurniture.map((item, index) => (
+            <FurnitureCard
+              key={index}
+              productId={item.id}
+              imageSrc={item.image_url}
+              label={item.name}
+              description={item.furniture_description}
+            />
+          ))
+        )}
       </div>
+      )}
     </div>
   );
 }
