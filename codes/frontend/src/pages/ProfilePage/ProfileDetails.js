5cfebadb34a6ef2a8bb4e7004fde48919f4c8c8e
Gautam Sagar <gautamsagar.mallela99@gmail.com>
2023-10-03 18:07:01 -0500

    User details in dashboard

--- codes/frontend/src/pages/ProfilePage/ProfileDetails.js
+++ codes/frontend/src/pages/ProfilePage/ProfileDetails.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Navbar from "../../components/Navbar/index";
 import Tab from "../../components/Tabs/Tabs";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./ProfileDetails.css";
+import UserDetails from "../../components/UserDetails/UserDetails";
+import { getUserDetails } from "../../utils/api";
 
 const ProfileDetails = () => {
   //code to enable scroll
@@ -16,9 +18,36 @@ const ProfileDetails = () => {
     };
   }, []);
 
+  const [userDetails, setUserDetails] = useState([]);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userData = await getUserDetails();
+
+        setUserDetails(userData);
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      }
+    };
+
+    fetchUserData();
+    return () => {
+      // Cleanup function (if needed)
+      // You can add any cleanup code here
+    };
+  }, []); // Empty dependency array means this effect runs only once on mount
+
   return (
     <div>
       <Navbar />
+
+      <UserDetails
+        first_name={userDetails.first_name}
+        last_name={userDetails.last_name}
+        email="johndoe@example.com"
+      />
+
       <Tab />
     </div>
   );
