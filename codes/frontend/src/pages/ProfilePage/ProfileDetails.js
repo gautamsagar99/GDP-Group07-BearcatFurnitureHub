f4ef5ec87a5c51f7432a88b4fd8198eef9352803
Gautam Sagar <gautamsagar.mallela99@gmail.com>
2023-10-03 17:53:09 -0500

    Resolved scroll issue from dashboard

--- codes/frontend/src/pages/ProfilePage/ProfileDetails.js
+++ codes/frontend/src/pages/ProfilePage/ProfileDetails.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import Navbar from '../../components/Navbar/index'
-import Tab from "../../components/Tabs/Tabs"
+import React from "react";
+import Navbar from "../../components/Navbar/index";
+import Tab from "../../components/Tabs/Tabs";
+import { useEffect } from "react";
+import "./ProfileDetails.css";
+
 const ProfileDetails = () => {
-	return (
-		<div>
-			<Navbar />
-			<Tab/>
-		</div>
-	);
+  //code to enable scroll
+  useEffect(() => {
+    // Set a class on the body when the component mounts
+    document.body.classList.add("custom-body-class-profilePage");
+
+    // Cleanup function to remove the class when the component unmounts
+    return () => {
+      document.body.classList.remove("custom-body-class-profilePage");
+    };
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <Tab />
+    </div>
+  );
 };
 
 export default ProfileDetails;
